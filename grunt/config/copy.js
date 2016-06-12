86814b525a4b7cbbe4fd0bc80c8fd04a74edbb86
Enrico Mraß <enrico.mrass@sap.com>
2016-06-12 16:30:32 +0200

    Prepare copy task for dist

--- grunt/config/copy.js
+++ grunt/config/copy.js
@@ -4,6 +4,26 @@
   module.exports = function(grunt, config) {
     var copy = {};
 
+    copy['dist'] = {
+      files: [{
+        expand: true,
+        dot: true,
+        cwd: config.paths.src,
+        src: [
+          // Jekyll processes and moves HTML and text files
+          // Usemin moves CSS and javascript inside usemin-blocks
+          // copy moves asset files and directories
+          'img/**/*',
+          'fonts/**/*',
+          // Like Jekyll, exclude files and folders prefixed with an _
+          '!**/_*{,/**}'
+          // Explicitly add any files the site needs for distribution here
+          // , '...'
+        ],
+        dest: config.paths.dist
+      }]
+    };
+
     copy['stageCss'] = {
       files: [{
         expand: true,
